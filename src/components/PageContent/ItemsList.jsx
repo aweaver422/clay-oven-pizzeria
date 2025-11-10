3af3a5ce368a66bc8ce696572536447e59bf4b6f
aweaver422 <aweaver422@gmail.com>
2025-11-10 00:28:03 -0500

    fixed another linking issue

--- src/components/PageContent/ItemsList.jsx
+++ src/components/PageContent/ItemsList.jsx
@@ -5,11 +5,12 @@ import OrderItem from "../PageContent/OrderItem";
 
 const Item = (props) => {
     const [items, setItems] = useState([]);
+    const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001";
     
     useEffect(()=>{
         const loadItems = async() => {
             try {
-                const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/menu/`);
+                const response = await axios.get(`${API_URL}/api/menu/`);
                 setItems(response.data.slice(0,props.num));
             } catch (error) {
                 console.error("Error loading menu: ", error)
@@ -25,7 +26,7 @@ const Item = (props) => {
                         key={item._id} 
                         id={item._id}
                         name={item.name} 
-                        img={`${process.env.REACT_APP_API_URL}${item.img}`}
+                        img={`${API_URL}${item.img}`}
                         type={item.type}
                         description={item.description}
                         price={item.price} 
